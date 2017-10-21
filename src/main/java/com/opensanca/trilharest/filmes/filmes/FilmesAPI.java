433dce70574f2f112d7bef41b4a30b76e5b25659
Unknown <Rafa_chuque@hotmail.com>
2017-10-21 09:18:30 -0200

    Commit 1

---
+++ src/main/java/com/opensanca/trilharest/filmes/filmes/FilmesAPI.java
@@ -0,0 +1,22 @@
+package com.opensanca.trilharest.filmes.filmes;
+
+import com.opensanca.trilharest.filmes.comum.Pagina;
+import com.opensanca.trilharest.filmes.comum.ParametrosDePaginacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/filmes")
+public class FilmesAPI {
+
+    @Autowired//essa anotaçao faz uma ligaçao automatica com uma anotaçao @Repository(entre outras anotaçoes)
+    private FilmesRepository filmesRepository;
+
+    @RequestMapping(path = "/em-exibicao", method = RequestMethod.GET)
+    public Pagina<Filme> getEmExibicao(ParametrosDePaginacao parametrosDePaginacao){
+        return filmesRepository.buscarPaginaEmExibicao(parametrosDePaginacao);
+    }
+
+}
